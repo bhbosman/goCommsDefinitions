2154014a189139a05a94017648c43df147030957
Brendan Bosman <bhbosman@gmail.com>
2023-03-13 19:52:54 +0200

    dd

--- provideCancelContext.go
+++ provideCancelContext.go
@@ -4,53 +4,8 @@ import (
 	"context"
 	"go.uber.org/fx"
 	"go.uber.org/zap"
-	"net"
 )
 
-func InvokeListenerClose() fx.Option {
-	return fx.Invoke(
-		func(
-			params struct {
-				fx.In
-				NetListener net.Listener
-				Lifecycle   fx.Lifecycle
-			},
-		) {
-			params.Lifecycle.Append(
-				fx.Hook{
-					OnStart: nil,
-					OnStop: func(ctx context.Context) error {
-						err := params.NetListener.Close()
-						return err
-					},
-				},
-			)
-		},
-	)
-}
-
-func InvokeCancelContext() fx.Option {
-	return fx.Invoke(
-		func(
-			params struct {
-				fx.In
-				Lifecycle           fx.Lifecycle
-				CancellationContext ICancellationContext
-			},
-		) error {
-			params.Lifecycle.Append(
-				fx.Hook{
-					OnStart: nil,
-					OnStop: func(ctx context.Context) error {
-						params.CancellationContext.Cancel()
-						return nil
-					},
-				},
-			)
-			return nil
-		},
-	)
-}
 func ProvideCancelContext(cancelContext context.Context) fx.Option {
 	return fx.Provide(
 		fx.Annotated{
